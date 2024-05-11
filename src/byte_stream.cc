777eb5519e843423655a59fe31474f7bc24b30c8
annv <annv@coccoc.com>
2024-05-11 17:54:18 +0700

    check0 - byte_stream_speed_test, Exception: Mismatch between data written and read

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <vector>
 #include "byte_stream.hh"
 
 using namespace std;
@@ -6,60 +8,72 @@ ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 
 bool Writer::is_closed() const
 {
-  // Your code here.
-  return {};
+  return closed_;
 }
 
 void Writer::push( string data )
 {
-  // Your code here.
-  (void)data;
-  return;
+  if (Writer::is_closed() || Writer::available_capacity() == 0 || data.empty()) {
+    return;
+  }
+  uint64_t remain = min(capacity_ - buffer_.size(), data.size());
+  uint64_t i = 0;
+  while (i < remain) {
+    buffer_.push_back(data[i]);
+    i++;
+  }
+  total_pushed_ += remain;
 }
 
 void Writer::close()
 {
-  // Your code here.
+  closed_ = true;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  // Your code here.
-  return {};
+  return capacity_ - buffer_.size();
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  // Your code here.
-  return {};
+  return total_pushed_;
 }
 
 bool Reader::is_finished() const
 {
-  // Your code here.
-  return {};
+  return closed_ && buffer_.empty();
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  // Your code here.
-  return {};
+  return total_popped_;
 }
 
 string_view Reader::peek() const
 {
-  // Your code here.
-  return {};
+  if (buffer_.empty()) return std::string_view {};
+  std::string tmp = "";
+  for (size_t i = 0; i < buffer_.size(); i++) {
+    tmp += buffer_[i];
+  }
+  std::string_view out {tmp};
+  return out;
 }
 
 void Reader::pop( uint64_t len )
 {
-  // Your code here.
-  (void)len;
+  if (buffer_.empty()) return;
+  uint64_t remain = min(len, buffer_.size());
+  uint64_t i = 0;
+  while (i < remain) {
+    buffer_.pop_front();
+    i++;
+  }
+  total_popped_ += remain;
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  // Your code here.
-  return {};
+  return buffer_.size();
 }
