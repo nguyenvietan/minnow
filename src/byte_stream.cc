d388f34581331f8d6b8eae9302633b7d96520d05
annv <annv@coccoc.com>
2024-07-31 23:42:52 +0700

    check0: re-implemented myself

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -3,119 +3,74 @@
 #include <iostream>
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity )
-  : preview(), stream_(), capacity_( capacity ), available_capacity_( capacity ), error_( false )
-{
-  preview = ""sv;
-}
+ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 
 bool Writer::is_closed() const
 {
-  // 返回文件流是否关闭
-  return is_closed_;
+  return close_;
 }
 
 void Writer::push( string data ) noexcept
 {
-  // Your code here.
-  if ( is_closed_ || available_capacity_ <= 0 || !data.length() ) {
-    return;
-  }
-  if ( data.length() > available_capacity() ) {
-    data.resize( available_capacity() );
-  }
-  available_capacity_ -= data.length();
-  total_bytes_pushed_ += data.length();
-  stream_.emplace( move( data ) );
-  if ( !stream_.empty() && preview.empty() ) {
-    preview = stream_.front();
-  }
-  return;
+    if (close_ || available_capacity() == 0 || data.empty()) return;
+    size_t L = min(available_capacity(), data.size());
+    deque_.push_back(data.substr(0, L));
+    total_bytes_pushed += L;
+    bytes_in_buffer += L;
 }
 
 void Writer::close()
 {
-  // Your code here.
-  if ( !is_closed_ ) {
-    is_closed_ = true;
-    // stream_.emplace( string( 1, EOF ) );
-  }
+  close_ = true;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  // Your code here.
-  return available_capacity_;
+  return capacity_ - bytes_in_buffer;
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  // Your code here.
-  return total_bytes_pushed_;
+  return total_bytes_pushed;
 }
 
 bool Reader::is_finished() const
 {
-  // Your code here.
-  return available_capacity_ == capacity_ && is_closed_;
+  return close_ && deque_.empty();
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  // Your code here.
-  return total_bytes_popped_;
+  return total_bytes_popped;
 }
 
 string_view Reader::peek() const
 {
-  // Your code here.
-  // return {};
-  // *str =  string(1, stream_.front());
-  // return string_view(*str);
-  // if(stream_.empty())
-  // {
-  //   preview = empty_str;
-  // }
-  return preview;
-  // return string_view(string(stream_.begin(), stream_.end()));
+  return string_view { deque_.front() };
 }
 
 void Reader::pop( uint64_t len )
 {
-  // Your code here.
-  if ( len > capacity_ - available_capacity_ ) {
-    return;
-  }
-  available_capacity_ += len; // pop了len 所以可用空间多len
-  total_bytes_popped_ += len;
-  while ( len > 0 ) {
-    if ( len >= preview.size() ) {
-      len -= preview.size();
-      stream_.pop();
-      if ( stream_.empty() ) {
-        preview = ""sv;
-      } else {
-        preview = stream_.front();
-      }
+  uint64_t bytes_popped = 0;
+  for (; bytes_popped < len; ) {
+    string tmp = deque_.front();
+    if (bytes_popped + tmp.size() <= len) {
+      deque_.pop_front();
+      bytes_popped += tmp.size();
+      bytes_in_buffer -= tmp.size();
     } else {
-      preview.remove_prefix( len );
-      len = 0;
+      uint64_t remaining = bytes_popped + tmp.size() - len;
+      string x = tmp.substr(tmp.size() - remaining);
+      deque_.pop_front();
+      deque_.push_front(x);
+      bytes_in_buffer -= tmp.size() - remaining;
+      bytes_popped += tmp.size() - remaining;
     }
-    if ( preview.size() == 0 ) {
-      break;
-    }
-  }
-  // if ( len >= preview.size() ) {
-  //   stream_.pop();
-  //   preview = stream_.empty() ? ""sv : stream_.front();
-  // } else
-  //   preview.remove_prefix( len );
-  // available_capacity_ += len;
-  // total_bytes_popped_ += len;
+  }  
+  total_bytes_popped += bytes_popped;
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  // Your code here.
-  return capacity_ - available_capacity_;
+  return bytes_in_buffer;
 }
