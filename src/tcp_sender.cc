799e2f6c8de07631187b1451dc25d260b9859ba6
annv <annv@coccoc.com>
2024-09-02 23:17:38 +0700

    check3: send_retx passed

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -3,6 +3,32 @@
 
 using namespace std;
 
+void Timer::reset()
+{
+  ms_since_beginning_ = 0;
+  cur_RTO_ms_ = initial_RTO_ms_;
+}
+
+void Timer::set_ms_since_begining( uint64_t x )
+{
+  ms_since_beginning_ = x;
+}
+
+void Timer::tick( uint64_t ms_since_last_tick )
+{
+  ms_since_beginning_ += ms_since_last_tick;
+}
+
+void Timer::double_RTO()
+{
+  cur_RTO_ms_ <<= 1;
+}
+
+bool Timer::is_timeout()
+{
+  return ms_since_beginning_ >= cur_RTO_ms_;
+}
+
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
   return cnt_seq_in_flight_;
@@ -10,24 +36,35 @@ uint64_t TCPSender::sequence_numbers_in_flight() const
 
 uint64_t TCPSender::consecutive_retransmissions() const
 {
-  // Your code here.
-  return {};
+  return cnt_consecutive_retx_;
 }
 
 void TCPSender::push( const TransmitFunction& transmit )
 {
+  TCPSenderMessage msg {};
+
   auto& reader = input_.reader();
 
   std::string_view content = reader.peek();
+
+  if ( reader.is_finished() && buffer_.empty() && window_size_ > 0) {
+    msg.FIN = true;
+    transmit( msg );
+    return;
+  }
+
   if ( content.empty() && sent_syn_ )
     return;
 
+  std::string sent_content = std::string( content );
 
-  TCPSenderMessage msg {};
-  msg.payload = content;
+  if ( window_size_ < content.size() ) {
+    sent_content = content.substr( 0, window_size_ );
+  }
+
   msg.SYN = !sent_syn_;
   msg.seqno = Wrap32::wrap( next_abs_seqno_, isn_ );
-
+  msg.payload = sent_content;
   transmit( msg );
 
   sent_syn_ |= true;
@@ -35,8 +72,9 @@ void TCPSender::push( const TransmitFunction& transmit )
   buffer_.emplace( msg );
   next_abs_seqno_ += msg.sequence_length();
   cnt_seq_in_flight_ += msg.sequence_length();
+  window_size_ -= sent_content.size();
 
-  reader.pop( content.size() );
+  reader.pop( sent_content.size() );
 }
 
 TCPSenderMessage TCPSender::make_empty_message() const
@@ -57,12 +95,18 @@ void TCPSender::receive( const TCPReceiverMessage& msg )
     cnt_seq_in_flight_ -= oldest_msg.sequence_length();
     buffer_.pop();
   }
+  window_size_ = msg.window_size;
+  timer.reset();
+  cnt_consecutive_retx_ = 0;
 }
 
 void TCPSender::tick( uint64_t ms_since_last_tick, const TransmitFunction& transmit )
 {
-  // Your code here.
-  (void)ms_since_last_tick;
-  (void)transmit;
-  (void)initial_RTO_ms_;
+  timer.tick( ms_since_last_tick );
+  if ( timer.is_timeout() && !buffer_.empty() ) {
+    transmit( buffer_.front() );
+    timer.double_RTO();
+    timer.set_ms_since_begining( 0 );
+    cnt_consecutive_retx_++;
+  }
 }
